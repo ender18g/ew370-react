a39128a0670f943da4f42a11e6877f2cfd974bbd
Allan Elsberry <allan.elsberry@gmail.com>
2021-07-31 10:11:42 -0400

    moved to hashrouter

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { FirebaseAppProvider, SuspenseWithPerf } from 'reactfire';
 import { ChakraProvider } from '@chakra-ui/react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const firebaseConfig = {
 	apiKey: 'AIzaSyDinZuBXMQbPuXTrEdl4DGRVH-EPZdK5ug',
@@ -22,9 +22,9 @@ ReactDOM.render(
 	<React.StrictMode>
 		<ChakraProvider>
 			<FirebaseAppProvider firebaseConfig={firebaseConfig}>
-				<BrowserRouter>
+				<HashRouter>
 					<App />
-				</BrowserRouter>
+				</HashRouter>
 			</FirebaseAppProvider>
 		</ChakraProvider>
 	</React.StrictMode>,
