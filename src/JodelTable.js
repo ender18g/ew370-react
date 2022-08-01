b80e669d513102785bbd5dc0a5be9275a7a6ae35
Allan Elsberry <allanelsberry@Allans-Mac-mini.local>
2022-07-31 21:43:06 -0400

    added search feature to posts

--- src/JodelTable.js
+++ src/JodelTable.js
@@ -1,35 +1,49 @@
 import React from 'react';
-import { Flex, Box, Spinner, Text, Heading, Button, List, ListItem } from '@chakra-ui/react';
-import { useState } from 'react';
+import { Flex, Box, Input, Text, Button, List, ListItem } from '@chakra-ui/react';
+import { useState, useEffect } from 'react';
 import { DeleteIcon } from '@chakra-ui/icons';
 
 export default function MobileJodel(props) {
 	const { jodels, removeJodel } = props;
-	console.log(jodels);
+	const [ searchTerm, setsearchTerm ] = useState('');
+
+	useEffect(
+		() => {
+			if (searchTerm === '') return;
+		},
+		[ searchTerm ]
+	);
 	return (
-		<Box>
+		<Flex justifyContent={'center'}>
 			<Box bg="gray.50" p="3" borderRadius="md" my="4" mx={{ base: '5', md: '200' }}>
 				<Box justifyContent="center">
-					<Heading mb="3" size="sm" letterSpacing=".1em" fontWeight="400" color="teal">
-						Recent Jodels
-					</Heading>
-					<List spacing="3">
+					<Input
+						m="2"
+						maxW="200px"
+						placeholder="Filter Jodels"
+						onChange={(e) => {
+							setsearchTerm(e.target.value);
+						}}
+					/>
+					<List maxW={'500px'} spacing="3">
 						{Object.keys(jodels).reverse().map((k, i) => {
 							if (!jodels[k]['input']) return '';
-							return (
-								<JodelLine
-									key={k}
-									jodel={jodels[k]}
-									removeJodel={() => {
-										removeJodel(k);
-									}}
-								/>
-							);
+							if (jodels[k]['input'].includes(searchTerm)) {
+								return (
+									<JodelLine
+										key={k}
+										jodel={jodels[k]}
+										removeJodel={() => {
+											removeJodel(k);
+										}}
+									/>
+								);
+							}
 						})}
 					</List>
 				</Box>
 			</Box>
-		</Box>
+		</Flex>
 	);
 }
 
